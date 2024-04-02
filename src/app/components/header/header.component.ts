4ad176d8c086a1074f8bb5ec1de8a78b43642ff6
Arthur <arthur.rodrigues@protonmail.com>
2024-04-02 18:51:13 -0300

    Feature
    Finishing header mobile implementation;

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  openMenu() {
+    (document.querySelector('.nav-wrapper') as HTMLElement).style.transform = 'scaleX(1)';
+    document.body.style.overflow = 'hidden';
+    document.body.insertAdjacentHTML('beforeend', '<div class="backdrop"></div>');
+  }
+
+  closeMenu() {
+    (document.querySelector('.nav-wrapper') as HTMLElement).style.transform = 'scaleX(0)';
+    (document.querySelector('.backdrop') as HTMLElement)?.remove();
+    document.body.style.overflow = '';
+  }
 }
